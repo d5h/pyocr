3b74d49515f5abf59cf29d271b0249316801ec98
Dan Hipschman <dsh@linux.ucla.edu>
2012-11-04 18:38:55 -0800

    add cross-correlation to adjust for phase shift.

--- corr.py
+++ corr.py
@@ -4,6 +4,7 @@ import cv
 
 import numpy as np
 from scipy.interpolate import interp1d
+from scipy.signal import fftconvolve
 from scipy.stats.stats import pearsonr
 
 from cont import cont_angles, cont_deriv
@@ -19,8 +20,11 @@ def image_corr(i, j):
         ai = scale_size(ai, aj)
     di = cont_deriv(ai)
     dj = cont_deriv(aj)
-    plot(ai)
-    plot(aj)
+    #plot(ai, 'r', aj, 'b')
+    k = find_shift(ai, aj)
+    ai = phase_shift(ai, k)
+    di = phase_shift(di, k)
+    plot(ai, 'r', aj, 'b')
     return pearsonr(ai, aj)[0], pearsonr(di, dj)[0]
 
 def scale_size(a, s):
@@ -31,6 +35,16 @@ def scale_size(a, s):
     for i in range(nb):
         b[i] = y(i * (na - 1.) / (nb - 1))
     return b
+
+def find_shift(a, b):
+    c = fftconvolve(np.array(a), np.array(b[::-1]))  # Reverse for cross-correlation
+    return np.argmax(c) - len(b)
+
+def phase_shift(a, k):
+    n = len(a)
+    s = np.concatenate((a[k:], a[:k]))
+    assert len(s) == n
+    return s
 
 if __name__ == '__main__':
     import sys
